f05931420193d009ec375bfa684df65a2adea537
pragatimrinal <prmrinal@in.ibm.com>
2017-07-20 13:44:24 +0530

    commit to insert table and read from table

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -21,7 +21,10 @@ import (
 	"fmt"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/op/go-logging"
 )
+
+var myLogger = logging.MustGetLogger("blockInABrick_mgm")
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
@@ -41,48 +44,89 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
-	}
-	err := stub.PutState("hello_world", []byte(args[0]))
-	if err != nil {
-		return nil, err
-	}
 
+	myLogger.Debug("Init Chaincode...")
+	if len(args) != 0 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 0")
+	}
+	// Create Stock table
+	err := stub.CreateTable("Brick_Item", []*shim.ColumnDefinition{
+		&shim.ColumnDefinition{Name: "Item_ID", Type: shim.ColumnDefinition_STRING, Key: true},
+		&shim.ColumnDefinition{Name: "Unit_Price", Type: shim.ColumnDefinition_STRING, Key: false},
+		&shim.ColumnDefinition{Name: "Item_Quantity", Type: shim.ColumnDefinition_STRING, Key: false},
+	})
+	if err != nil {
+		return nil, errors.New("Failed creating Brick_Item table.")
+	}
+	myLogger.Debug(" Init Done.")
 	return nil, nil
 }
 
 // Invoke is our entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("invoke is running " + function)
-
+	myLogger.Debug("Invoke  Chaincode...")
 	// Handle different functions
 	if function == "init" { //initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
-	} else {
-		return t.write(stub, args)
+	} else if function == "insertItem" { //generic read ledger
+		return t.insertItem(stub, args)
 	}
-	fmt.Println("invoke did not find func: " + function) //error
+	myLogger.Debug(" Invoke Done.")
 
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
-//write the value in stub
-func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, value string
-	var err error
-	fmt.Println("running write()")
-	if len(args) != 2 {
-		return nil, errors.New("Expecting key ,value 2 arguments as input")
+//insert item in the Item table
+func (t *SimpleChaincode) insertItem(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
 	}
-	key = args[0]
-	value = args[1]
+	Item_ID := args[0]
+	Unit_Price := args[1]
+	Item_Quantity := args[2]
 
-	stub.PutState(key, []byte(value))
+	var columns []shim.Column
+	idCol := shim.Column{Value: &shim.Column_String_{String_: Item_ID}}
+	columns = append(columns, idCol)
+	row, err := stub.GetRow("Brick_Item", columns)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Failed retriving associated row [%s]", err)
 	}
-	return nil, nil
+
+	if len(row.Columns) == 0 {
+		// Insert row
+		ok, err := stub.InsertRow("Brick_Item", shim.Row{
+			Columns: []*shim.Column{
+				&shim.Column{Value: &shim.Column_String_{String_: Item_ID}},
+				&shim.Column{Value: &shim.Column_String_{String_: Unit_Price}},
+				&shim.Column{Value: &shim.Column_String_{String_: Item_Quantity}},
+			},
+		})
+		if err != nil {
+			return nil, fmt.Errorf("Failed inserting row [%s]", err)
+		}
+		if !ok {
+			return nil, errors.New("Failed inserting row.")
+		}
+
+	} else {
+		// Update row
+		ok, err := stub.ReplaceRow("Brick_Item", shim.Row{
+			Columns: []*shim.Column{
+				&shim.Column{Value: &shim.Column_String_{String_: row.Columns[0].GetString_() + " " + Item_ID}},
+				&shim.Column{Value: &shim.Column_String_{String_: row.Columns[1].GetString_() + " " + Unit_Price}},
+				&shim.Column{Value: &shim.Column_String_{String_: row.Columns[2].GetString_() + " " + Item_Quantity}},
+			},
+		})
+		if err != nil {
+			return nil, fmt.Errorf("Failed replacing row [%s]", err)
+		}
+		if !ok {
+			return nil, errors.New("Failed replacing row.")
+		}
+	}
+
+	return nil, err
 }
 
 // Query is our entry point for queries
@@ -90,8 +134,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "read" {
-		return t.read(stub, args)
+	if function == "readItem" {
+		return t.readItem(stub, args)
 	}
 
 	fmt.Println("query did not find func: " + function) //error
@@ -100,17 +144,23 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 }
 
 //read from stub.
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, jsonResp string
+func (t *SimpleChaincode) readItem(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var err error
 	if len(args) != 1 {
 		return nil, errors.New("Expecting one argument as key")
 	}
-	key = args[0]
-	ValAsByte, err := stub.GetState(key)
+	item_code := args[0]
+	var columns []shim.Column
+	col1 := shim.Column{Value: &shim.Column_String_{String_: item_code}}
+	columns = append(columns, col1)
+
+	row, err := stub.GetRow("Brick_Item", columns)
 	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonResp)
+		myLogger.Debugf("Failed retriving item_code [%s]: [%s]", string(item_code), err)
+		return nil, fmt.Errorf("Failed retriving item_code [%s]: [%s]", string(item_code), err)
 	}
-	return ValAsByte, nil
+
+	myLogger.Debugf("Query done [% x]", row.Columns[1].GetBytes())
+
+	return row.Columns[1].GetBytes(), nil
 }
